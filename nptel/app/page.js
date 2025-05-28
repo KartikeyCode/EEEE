4cb55fbd7df3103b4fce00163cd92d35c2e6f2c7
CodeKartikey <codekartikey@gmail.com>
2025-05-28 20:26:18 +0530

    add pdf

--- nptel/app/page.js
+++ nptel/app/page.js
@@ -4,7 +4,7 @@ export default function CertificateButton() {
   return (
     <div className={styles.container}>
       <a
-        href="/NOC/NOC25/SEM1/Ecertificates/106/noc25-cs11/Course/NPTEL25CS11S84320371404245990.pdf"
+        href="/NPTEL25CS11S843204694.pdf"
         target="_blank"
         className={styles.button}
       >
